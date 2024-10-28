35cf56775a1680b0f059a4400c97a885d37e6077
DaveM051 <merigildod3205@masseyhigh.school.nz>
2024-10-29 11:39:47 +1300

    Add files via upload

--- B_01_factors.py
+++ B_01_factors.py
@@ -24,23 +24,51 @@ If you want to exit the program please enter 'xxx'.
 
 # Ask user for an integer between 1 and 200.
 def num_check(question):
-    error = "Please enter a number that is more than zero\n"
-
+    error = "Please enter a number that is between 1 and 200 inclusive\n"
     while True:
 
+        response = input(question).lower()
+        if response == "xxx":
+            return response
+
         try:
-
             # ask the user for a number
-            response = float(input(question))
+            response = int(response)
 
-            # check that the number is more than zero
-            if response > 0:
+            # check that the number is between 1 and 200
+            if 1 <= response <= 200:
                 return response
             else:
                 print(error)
 
         except ValueError:
             print(error)
+
+
+# Works out factors, returns sorted list
+def factor(var_to_factor):
+    factors_list = []
+
+    # square root the number to work out when to stop looping.
+    stop = var_to_factor ** 0.5
+    stop = int(stop)
+
+    for item in range(1, stop + 1):
+
+        # check to see if the item is a factor
+        if to_factor % item == 0:
+            factors_list.append(item)
+
+            # Calculate partner
+            partner = to_factor // item
+
+            # Add partner to the list (but prevent duplicate entries)
+            if partner != item:
+                factors_list.append(partner)
+
+    # return the sorted list
+    factors_list.sort()
+    return factors_list
 
 
 # Main Routine Goes Here
@@ -55,8 +83,44 @@ if want_instructions == "":
     instructions()
 
 while True:
-    to_factor = num_check("To factor: ")
-    print("You chose to factor", to_factor)
+
+    comment = ""
+
+    # ask user for number to be factorised
+    to_factor = num_check("\nEnter an factor (or xxx to quit): ")
 
     if to_factor == "xxx":
         break
+
+    # get factors for integers that are 2 or more
+    elif to_factor != 1:
+        all_factors = factor(to_factor)
+
+    # Set up comment for unity
+    else:
+        all_factors = ""
+        comment = "One is UNITY!  It only has one factor.  Itself :)"
+
+    # comments for squares / primes
+
+    # Prime numbers have only two factors
+    if len(all_factors) == 2:
+        comment = f"{to_factor} is a prime number"
+
+    # check if the list has an odd number of factors
+    elif len(all_factors) % 2 == 1:
+        comment = f"{to_factor} is a perfect square"
+
+    # Set up headings
+    if to_factor > 1:
+        heading = f"Factors of {to_factor}"
+    else:
+        heading = "One is special..."
+
+    # output factors and comment
+    print()
+    statement_generator(heading, "*")
+    print(all_factors)
+    print(comment)
+
+print("Thank you for using the factors calculator")
